dbe70c5db0bf9d17a150fc8dcee5a0da3e91817e
ZahrizhalAli <zzahrizhal.ali@gmail.com>
2023-01-04 14:24:32 +0800

    Save address on checkout

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -2,9 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { emptyUserCart, getUserCart, saveUserAddress } from '../functions/user';
 import { toast } from 'react-toastify';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 function Checkout() {
   const [products, setProducts] = useState([]);
   const [total, setTotal] = useState(0);
+  const [address, setAddress] = useState('');
+  const [addressSaved, setAddressSaved] = useState(false);
+
   const dispatch = useDispatch();
   const { user } = useSelector((state) => ({ ...state }));
 
@@ -35,16 +40,21 @@ function Checkout() {
   };
 
   const saveAddressToDB = () => {
-    //
+    saveUserAddress(user.token, address).then((res) => {
+      if (res.data.ok) {
+        setAddressSaved(true);
+        toast.success('Address saved');
+      }
+    });
   };
   return (
     <>
       <div className="row">
-        <div className="col-md-6">
+        <div className="col-md-6 pl-4">
           <h4>Delivery Address</h4>
           <br />
           <br />
-          textarea
+          <ReactQuill theme="snow" value={address} onChange={setAddress} />
           <button className="btn btn-primary mt-2" onClick={saveAddressToDB}>
             Save
           </button>
@@ -74,7 +84,12 @@ function Checkout() {
 
           <div className="row">
             <div className="col-md-6">
-              <button className="btn btn-primary">Place Order</button>
+              <button
+                disabled={!addressSaved || !products.length}
+                className="btn btn-primary"
+              >
+                Place Order
+              </button>
             </div>
             <div className="col-md-6">
               <button
