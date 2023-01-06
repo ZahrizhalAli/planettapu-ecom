3bebba21a8d45287d71a20e464794d843aa0c8d5
ZahrizhalAli <zzahrizhal.ali@gmail.com>
2023-01-06 14:14:47 +0800

    Coupon create function in page

--- src/pages/admin/coupon/CreateCoupon.js
+++ src/pages/admin/coupon/CreateCoupon.js
@@ -13,8 +13,25 @@ import { DeleteOutlined } from '@ant-design/icons';
 import AdminNav from '../../../components/nav/AdminNav';
 function CreateCoupon() {
   const [name, setName] = useState('');
-  const handleSubmit = () => {
+  const [expiry, setExpiry] = useState('');
+  const [discount, setDiscount] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const { user } = useSelector((state) => ({ ...state }));
+  const handleSubmit = (e) => {
     //
+    e.preventDefault();
+    createCoupons({ name, expiry, discount }, user.token)
+      .then((res) => {
+        setLoading(false);
+        setName('');
+        setDiscount('');
+        setExpiry('');
+        toast.success(`${res.data.name} is created`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   return (
     <>
@@ -26,27 +43,54 @@ function CreateCoupon() {
             <AdminNav />
 
             <div class="col-md-9 col-sm-7">
-              <h1>Sub Category</h1>
+              <h1>Coupons</h1>
               <div class="content-page">
                 <form class="form-signin" onSubmit={handleSubmit}>
-                  <label for="inputEmail">New Category</label>
-                  <input
-                    size="50"
-                    type="text"
-                    id="inputEmail"
-                    class="form-control"
-                    placeholder="Enter new Category"
-                    value={name}
-                    onChange={(e) => {
-                      setName(e.target.value);
-                    }}
-                    autofocus
-                  />
+                  <div className="form-group">
+                    <label for="inputEmail">New Coupon</label>
+                    <input
+                      size="50"
+                      type="text"
+                      id="inputEmail"
+                      class="form-control"
+                      placeholder="Create New Coupon"
+                      value={name}
+                      onChange={(e) => {
+                        setName(e.target.value);
+                      }}
+                      autofocus
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="inputEmail">Discount %</label>
+                    <input
+                      size="50"
+                      type="text"
+                      id="inputEmail"
+                      class="form-control"
+                      placeholder="Create New Coupon"
+                      value={discount}
+                      onChange={(e) => {
+                        setDiscount(e.target.value);
+                      }}
+                      autofocus
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="inputEmail">Expiry Date</label>
+                    <DatePicker
+                      className="form-control"
+                      selected={new Date()}
+                      value={expiry}
+                      required
+                      onChange={(date) => setExpiry(date)}
+                    />
+                  </div>
                   <button
                     class="btn btn-md btn-outline-dark btn-category"
                     type="submit"
                   >
-                    Add Sub Category
+                    Add Coupon
                   </button>
                 </form>{' '}
                 <hr />
